83a7e65c477e07fbc626ad354b2d21a778a6c5a6
GrigoriyKocharyan <98645200+GrigoriyKocharyan@users.noreply.github.com>
2022-10-01 15:48:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,25 +6,45 @@ import re
 saved2 = 0
 
 
+red = 0
+blue = 0
 cs = 0
-
+green = 0
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-yyy = 500
+ggg = 680
+yyy = 680
 PixelsS = ''
 Pixels = []
 files = ''
+bbb = 680
 
-
-def colorSelect(yyy):
+def colorSelect(yyy,redtt,cs,ggg,redtttt,redtttt2,bbb,red,green,blue):
     draw.rect(win, (235, 235, 235), (25, 480, 200, 250), 100, 10)
     draw.rect(win, (215, 215, 215), (25, 480, 200, 250), 3, 10)
 
     draw.rect(win, (215, 100, 100), (25+20, 480+20, 15, 200), 3, 10)
     draw.rect(win, (100, 215, 100), (25 + 60, 480 + 20, 15, 200), 3, 10)
     draw.rect(win, (100, 100, 215), (25 + 100, 480 + 20, 15, 200), 3, 10)
+
+    draw.rect(win, (red, green, blue), (25 + 100+50, 480 + 20, 30, 200), 0, 15)
+    draw.rect(win, (red/1.4, green/1.4, blue/1.4), (25 + 100 + 50, 480 + 20, 30, 200), 3, 15)
+
     draw.rect(win, (255, 0, 0), (25 + 18, yyy, 19, 19), 0, 10)
     draw.rect(win, (215,100, 100), (25 + 18, yyy, 19, 19), 3, 10)
+    win.blit(redtt, (-redtt.get_rect()[2]/2+53,705))
 
+    draw.rect(win, (0, 255, 0), (25 + 18+19+19+2, ggg, 19, 19), 0, 10)
+    draw.rect(win, (100, 215, 100), (25 + 18+19+19+2, ggg, 19, 19), 3, 10)
+    win.blit(redtttt, (-redtttt.get_rect()[2] / 2 + 53+38, 705))
+
+    draw.rect(win, (0, 0, 255), (25 + 18 + 19 + 19 + 2+38+2, bbb, 19, 19), 0, 10)
+    draw.rect(win, (100, 100, 215), (25 + 18 + 19 + 19 + 2+38+2, bbb, 19, 19), 3, 10)
+    win.blit(redtttt2, (-redtttt2.get_rect()[2] / 2 + 53 + 38+39+2, 705))
+
+
+    #return ggg, bbb,yyy
+    #return bbb
+    #return yyy
 
 
     #display.flip()
@@ -35,7 +55,7 @@ sfile = open("MyFile.mpi", 'w+', encoding='utf-8')
 def fileSave():
 
     sfile.write(str(PixelsS))
-    print(str(PixelsS))
+
 
 p = image.load("pencil.png")
 p = transform.smoothscale(p, (25, 25))
@@ -49,6 +69,10 @@ r, g, b = 40, 0, 50
 init()
 win = display.set_mode((800, 800))
 
+redt = font.SysFont('Lucida Console', 15,1,0)
+redtt = redt.render(str(red), 1, (0, 0, 0))
+redtttt = redt.render(str(green), 1, (0, 0, 0))
+redtttt2 = redt.render(str(blue), 1, (0, 0, 0))
 
 class Pixel:
     def __init__(self, x, y, win, size, r, g, b, is_earser):
@@ -72,8 +96,16 @@ while True:
     win.fill((235,235,235))
     draw.rect(win, (255, 255, 255), (15+35, 15, 700, 700))
 
+    if cs == 1:
+        red = int(-yyy * 1.39+683+266+1)
+        green = int(-ggg * 1.39 + 683 + 266 + 1)
+        blue = int(-bbb * 1.39 + 683 + 266 + 1)
 
+        redtt = redt.render(str(red), 1, (0, 0, 0))
+        redtttt = redt.render(str(green), 1, (0, 0, 0))
+        redtttt2 = redt.render(str(blue), 1, (0, 0, 0))
 
+    print(redtt.get_rect())
     for i in event.get():
         if i.type == QUIT:
             exit()
@@ -95,10 +127,18 @@ while True:
         pix.draw()
 
     if mouse.get_pressed()[0]:
-        if mouse.get_pos()[0] >= 25 + 18 and mouse.get_pos()[0] <= 25 + 18 +19:
+        if mouse.get_pos()[0] >= 25 + 18 and mouse.get_pos()[0] <= 25 + 18 +19 and cs:
             yyy = min(max(500, mouse.get_pos()[1]), 480 + 203)
-            if mouse.get_pressed()[0]:
+            if mouse.get_pressed()[0] and cs:
                 yyy = min(max(500, mouse.get_pos()[1]), 480 + 203)
+        if mouse.get_pos()[0] >= 25 + 18+19+19 and mouse.get_pos()[0] <= 25 + 18 + 19+18+19 and cs:
+            ggg = min(max(500, mouse.get_pos()[1]), 480 + 203)
+        if mouse.get_pos()[0] >= 25 + 18+19+19+38 and mouse.get_pos()[0] <= 25 + 18 + 19+18+19+38 and cs:
+            bbb = min(max(500, mouse.get_pos()[1]), 480 + 203)
+
+
+
+
 
 
     if m[0] >= 25 and m[0] <= 65 and m[1] >= 740 and m[1] <= 780:
@@ -127,7 +167,7 @@ while True:
 
 
     if cs:
-        colorSelect(yyy)
+        colorSelect(yyy,redtt,cs,ggg,redtttt,redtttt2,bbb,red,green,blue)
 
     if saved2 == 0:
         if pencil == 1:
@@ -153,7 +193,7 @@ while True:
                 draw.rect(win, (255, 255, 255), (round(m[0] / size) * size + 1, round(m[1] / size) * size - size / 2 + 1, size - 2, size - 2), 1)
 
             if mouse.get_pressed()[0] and cs == 0:
-                Pixels.append(Pixel(xx, yy, win, size, r, g, b, -pencil+1))
+                Pixels.append(Pixel(xx, yy, win, size, red, green, blue, -pencil+1))
 
                 PixelsS += ("x:"+ str(int(xx)) + ",")
                 PixelsS += ("y:" + str(int(yy)) + ",")
